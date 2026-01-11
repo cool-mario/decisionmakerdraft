a2c1b14c27fd9f5fe0d1a5b54208c83c541cd548
Aidan Chan <50786617+cool-mario@users.noreply.github.com>
2026-01-10 16:30:08 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/plinko-decision-maker/',
+  base: '/decisionmakerdraft/',
   server: {
     host: "::",
     port: 8080,
